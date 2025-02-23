feee4f477025f37fa2f07aa494c2e12502707aee
Ammar Hasan <ammar.hasan37@qmail.cuny.edu>
2025-02-23 02:07:51 -0500

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Menubar from "@/components/Menubar";
+import Menubar from "@/Components/Menubar";
 
 
 const geistSans = Geist({
